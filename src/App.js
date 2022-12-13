21daad8d0b8ee9ee21a206fd38051d960ce8ded7
Simeodey <adamand12@abv.bg>
2022-12-13 06:45:44 +0200

    SD

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState('')
+
+  useEffect(()=>{
+    setText(localStorage.getItem('savedText'))
+  },[])
+
+  const handleChange = useCallback((e)=>{
+    setText(e.target.value)
+  },[])
+  const handleClear = useCallback(()=>{
+    setText('')
+  },[])
+
+  const onSave = ()=>{
+    console.log(text)
+    localStorage.setItem('savedText', text)
+  }
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea className="textarea is-large" placeholder="Notes..." onChange={handleChange} value = {text}/>
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active" onClick={onSave}>Save</button>
+        <button className="button is-large" onClick = {handleClear}>Clear</button>
       </div>
     </div>
   );
